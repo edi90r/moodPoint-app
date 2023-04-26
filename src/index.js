8ca10d7b533fb75ff7488e11f54d04f2af38c211
edi90r <30602529+edi90r@users.noreply.github.com>
2023-04-26 18:51:13 +0200

    Update index.js
    delete basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/app">
+  <Router>
     <Root />
   </Router>
 );
